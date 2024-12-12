7d5bfee0b69fa2ea2d11a8a472874b30700f5abc
stdonnelly <80422933+stdonnelly@users.noreply.github.com>
2024-12-12 14:04:46 -0500

    Add project files, merge sort, and vector

---
+++ README.md
@@ -0,0 +1,5 @@
+# C Data Structures
+
+This repository is my implementation of some common data structures in C, and some data structure operations, such as sorting.
+Most of these are influenced by either the C++ standard template library, or the Java standard library.
+To allow me to more easily use these structures, most of these will have a generic header file and some executable testing code.
---
+++ vector/vector_template.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Macro to define a generic vector
+#define DEF_VEC(T)                                               \
+    typedef struct T##_Vec                                       \
+    {                                                            \
+        T *arr;                                                  \
+        size_t len;                                              \
+        size_t cap;                                              \
+    } T##_Vec;                                                   \
+    void resize_##T##_Vec(T##_Vec *this, size_t new_cap)         \
+    {                                                            \
+        this->arr = realloc(this->arr, sizeof(T) * new_cap);     \
+        this->cap = new_cap;                                     \
+    }                                                            \
+    void append_##T##_Vec(T##_Vec *this, T val)                  \
+    {                                                            \
+        if (this->len + 1 > this->cap)                           \
+            resize_##T_Vec(this, this->cap ? this->cap * 2 : 1); \
+        *(this->arr + (this->len++)) = val;                      \
+    }                                                            \
+    T##_Vec new##T##_Vec() { return (T##_Vec){NULL, 0, 0}; }
